60331d488aa0072584d62605a36d04327dd74788
Jeronimo <jeronimocomaschi05@gmail.com>
2023-04-25 17:52:42 -0300

    restructurando el render del mapa

--- game.js
+++ game.js
@@ -17,17 +17,26 @@ function setCanvasSize() {
     canva.setAttribute('width', canvaSize)
     canva.setAttribute('height', canvaSize)
 
-    elementSize = (canvaSize / 10) - 1
+    elementSize = (canvaSize / 10)
 
     starGame()
 }
 
 function starGame() {
     game.font = elementSize + 'px Verdana'
-    game.textAlign = ""
-    
-    for (let i = 0; i < 10; i++) {
-        game.fillText(emojis['X'], elementSize * i, elementSize)
+    game.textAlign = 'end'
 
-    }
+    const map = maps[0];
+    const mapRows = map.trim().split('\n')
+    const mapCols = mapRows.map(row => row.trim().split(''))
+    
+    mapCols.forEach((row, rowI) =>
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementSize * (colI + 1)
+            const posY = elementSize * (rowI + 1)
+            game.fillText(emoji, posX, posY)
+        }))
+
+    return {mapRows, mapCols}
 }
