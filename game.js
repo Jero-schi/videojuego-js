3c32fa991f042ca54a21fe4c96c3662fa724c2fd
Jeronimo <jeronimocomaschi05@gmail.com>
2023-04-25 16:00:23 -0300

    haciendo resize constante al canvas

--- game.js
+++ game.js
@@ -2,24 +2,27 @@
 const canva = document.querySelector('#game');
 const game = canva.getContext('2d');
 
-window.addEventListener('load', starGame)
+let canvaSize;
+let elementSize;
 
-function starGame() {
-    let canvaSize;
 
-    if (window.innerHeight > window.innerWidth) {
-        canvaSize = window.innerWidth * 0.8
-    } else {
-        canvaSize = window.innerHeight * 0.6
-    }
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
+
+function setCanvasSize() { 
+    window.innerHeight > window.innerWidth
+        ? canvaSize = window.innerWidth * 0.8
+        : canvaSize = window.innerHeight * 0.6
 
     canva.setAttribute('width', canvaSize)
     canva.setAttribute('height', canvaSize)
 
-    const elementSize = (canvaSize / 10) - 1
+    elementSize = (canvaSize / 10) - 1
 
-    console.log({canvaSize, elementSize});
+    starGame()
+}
 
+function starGame() {
     game.font = elementSize + 'px Verdana'
     game.textAlign = ""
     
@@ -27,7 +30,4 @@ function starGame() {
         game.fillText(emojis['X'], elementSize * i, elementSize)
 
     }
-
-//     game.fillRect(0,0,100,100);
-//     game.clearRect(50,0,50,50);
 }
