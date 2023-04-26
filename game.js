c1b1459acaa354d0673a5a98d947609435b98824
Jeronimo <jeronimocomaschi05@gmail.com>
2023-04-26 17:36:29 -0300

    colicion con la meta

--- game.js
+++ game.js
@@ -1,65 +1,78 @@
-{HTMLCanvasElement}
 const canva = document.querySelector('#game');
+const game = canva.getContext('2d');
 const btnUp = document.querySelector('#up')
 const btnLeft = document.querySelector('#left')
 const btnRight = document.querySelector('#right')
 const btnDown = document.querySelector('#down')
-const game = canva.getContext('2d');
-
 let canvaSize;
 let elementSize;
-
 const playerPosition = {
     x: undefined,
-    y: undefined
+    y: undefined,
+};
+const giftPosition = {
+    x: undefined,
+    y: undefined,
 };
 
-
-window.addEventListener('load', setCanvasSize)
-window.addEventListener('resize', setCanvasSize)
-
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 function setCanvasSize() { 
     window.innerHeight > window.innerWidth
         ? canvaSize = window.innerWidth * 0.8
-        : canvaSize = window.innerHeight * 0.6
+        : canvaSize = window.innerHeight * 0.8
 
-    canva.setAttribute('width', canvaSize)
-    canva.setAttribute('height', canvaSize)
+    canva.setAttribute('width', canvaSize);
+    canva.setAttribute('height', canvaSize);
 
-    elementSize = (canvaSize / 10)
+    elementSize = canvaSize / 10;
 
-    starGame()
+    startGame();
 }
 
-function starGame() {
-    game.font = elementSize + 'px Verdana'
-    game.textAlign = 'end'
-
-    const map = maps[0];
-    const mapRows = map.trim().split('\n')
-    const mapCols = mapRows.map(row => row.trim().split(''))
+function startGame(level=0) {
+    game.font = elementSize + 'px Verdana';
+    game.textAlign = 'end';
+    const map = maps[level];
+    const mapRows = map.trim().split('\n');
+    const mapCols = mapRows.map(row => row.trim().split(''));
     
     game.clearRect(0,0,canvaSize, canvaSize)
-    mapCols.forEach((row, rowI) =>
+    mapCols.forEach((row, rowI) => {
         row.forEach((col, colI) => {
             const emoji = emojis[col];
-            const posX = elementSize * (colI + 1)
-            const posY = elementSize * (rowI + 1)
+            const posX = elementSize * (colI + 1);
+            const posY = elementSize * (rowI + 1);
 
             if (col == 'O') {
                 if (!playerPosition.x && !playerPosition.y) {
-                    playerPosition.x = posX
-                    playerPosition.y = posY
+                    playerPosition.x = posX;
+                    playerPosition.y = posY;
                     console.log({playerPosition});
                 }
+            } else if (col == 'I') {
+                giftPosition.x = posX
+                giftPosition.y = posY
             }
 
             game.fillText(emoji, posX, posY)
-        }))
+        });
+    });
     movePLayer()
 }
 
 function movePLayer() {
+    const giftCollisionX = playerPosition.x.toFixed(3) == giftPosition.x.toFixed(3);
+    const giftCollisionY = playerPosition.y.toFixed(3) == giftPosition.y.toFixed(3);
+    const giftCollision = giftCollisionX && giftCollisionY;
+
+    console.log({ giftCollisionX, giftCollisionY, giftCollision, giftPosition, playerPosition});
+
+    if (giftCollision) {
+    console.log('Subiste de nivel!');
+    // startGame(1)
+    }
+
     game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y)
 }
 
@@ -95,23 +108,23 @@ function moveUp() {
     (playerPosition.y - elementSize) < 0
         ? console.log('out')
         : playerPosition.y -= elementSize
-        starGame()
+        startGame()
     }
 function moveLeft() {
     (playerPosition.x - elementSize) < elementSize
         ? console.log('out')
         : playerPosition.x -= elementSize
-        starGame()
+        startGame()
 }
 function moveRight() {
     (playerPosition.x + elementSize) > canvaSize
         ? console.log('out')
         : playerPosition.x += elementSize
-        starGame()
+        startGame()
 }
 function moveDown() {
     (playerPosition.y + elementSize) > canvaSize
         ? console.log('out')
         : playerPosition.y += elementSize
-        starGame()
+        startGame()
 }
